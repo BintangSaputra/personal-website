3ad4043d5b9699f9ef0b86199225205ed72e4fe1
BintangSaputra <bintangcbs26@gmail.com>
2019-05-11 14:20:12 +0700

    align text

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const HomeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
   margin: 300px 50px;
   z-index: 0;
 `;
@@ -10,7 +14,8 @@ export default class Home extends React.Component {
   render() {
     return (
       <HomeWrapper>
-        <h1>Hi, i'm Bintang Saputra</h1>
+        <h1>Hi, I'm Bintang Saputra</h1>
+        <h2>I am a Frontend Web Developer</h2>
       </HomeWrapper>
     );
   }
